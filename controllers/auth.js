14a1564d52ccbd91cf1f6fa7784c6758e30045c2
Birru Vamshi <83230026+Vamshi-Birru@users.noreply.github.com>
2024-07-24 10:41:22 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'None',
       })
       .status(200)
       .json({ details: { ...otherDetails }, isAdmin });
